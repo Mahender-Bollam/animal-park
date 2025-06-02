4867eaccc096bb71cae495f3913feeb8cb98925e
cheppali kalyani <kalyani.cheppali@everest.engineering>
2025-06-02 20:39:53 +0530

    Cat is created

---
+++ cat.py
@@ -0,0 +1,11 @@
+class Cat:
+    def eat(self):
+        print("Cat can Eat")
+
+    def sleep(self):
+        print("Cat can Sleep")
+
+    def sound(self):
+       print("Cat can Sound 'MEOW'")
+
+    
