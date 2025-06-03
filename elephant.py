22cbb442efddd8cfb156cedebfbdc13afc872a23
Ruchitha <your-noreply-email@users.noreply.github.com>
2025-06-03 12:22:47 +0530

    Elephant is created

---
+++ elephant.py
@@ -0,0 +1,9 @@
+class Elephant:
+    def __init__(self):
+        self.legs = 4
+        self.color = "gray"
+        self.has_trunk = True
+        self.sound = "trumpet"
+
+    def traits(self):
+        print("Elephants are amazing animals with long trunk and loud trumpet sounds.")
